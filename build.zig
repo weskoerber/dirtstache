283502a642f17350207521d583129acbe72af616
Wes Koerber <wes@weskoerber.com>
2024-09-04 23:38:42 -0400

    chore: split up spec tests again

--- build.zig
+++ build.zig
@@ -3,6 +3,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const test_step = b.step("test", "Run unit tests");
+    const test_spec_step = b.step("test-spec", "Run spec tests");
 
     const dirtstache = b.addModule("dirtstache", .{
         .root_source_file = b.path("src/root.zig"),
@@ -28,7 +29,7 @@ pub fn build(b: *std.Build) void {
     spec_test_exe.root_module.addImport("dirtstache", dirtstache);
 
     const run_spec_test_exe = b.addRunArtifact(spec_test_exe);
-    test_step.dependOn(&run_spec_test_exe.step);
+    test_spec_step.dependOn(&run_spec_test_exe.step);
 }
 
 const std = @import("std");
